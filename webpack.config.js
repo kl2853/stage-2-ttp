4bc9cc907ff22d536c37f5ac2ad4955b2cc4d290
Kat Le <kl2853@barnard.edu>
2020-02-19 18:28:35 -0500

    chore: set up babel and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const devMode = (process.env.NODE_ENV === "development");
+
+module.exports = {
+    mode: devMode ? "development" : "production",
+    entry: [
+        "@babel/polyfill",
+        "./client/index.js"
+    ],
+    output: {
+        path: __dirname,
+        filename: "./public/bundle.js"
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    devtool: "source-map",
+    watchOptions: {
+        ignored: /node_modules/
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            }
+        ]
+    }
+}
