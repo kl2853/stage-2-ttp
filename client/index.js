9cecb74bec6fadbeff0e851ad8bf5ab1de2f5ce0
Kat Le <kl2853@barnard.edu>
2020-02-20 15:12:42 -0500

    feat: add redux and connect with react

--- client/index.js
+++ client/index.js
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Router } from "react-router-dom";
 import history from "./history";
+import { Provider } from "react-redux";
+import store from "./store";
 import App from "./app";
 
 ReactDOM.render(
-    <Router history={history} >
-        <App />
-    </Router>,
+    <Provider store={store} >
+        <Router history={history} >
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById("app")
 )
