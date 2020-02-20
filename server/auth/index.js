5fd8aee3ae30eb3c26ecf356fc6722b598262e1e
Kat Le <kl2853@barnard.edu>
2020-02-20 12:49:59 -0500

    feat: add auth routes and middleware

---
+++ server/auth/index.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const User = require("../db/models/user");
+
+module.exports = router;
+
+router.post("/login", async(req, res, next) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+
+        if(!user) {
+            console.log("No user found for email: ", req.body.email);
+            res.status(401).send("This email is not associated with an account");
+        } else if(!user.validPassword(req.body.password)) {
+            console.log("Incorrect password for user: ", req.body.email);
+            res.status(401).send("The email or password is incorrect");
+        }
+    } catch (err) {
+        next(err);
+    }
+})
+
+router.post("/signup", async(req, res, next) => {
+    try {
+        const user = await User.create(req.body);
+        req.login(user, err => (err
+            ? next(err)
+            : res.json(user)
+        ));
+    } catch (err) {
+        if(err.name === "SequelizeUniqueConstraintError") {
+            res.status(401).send("This email is already in use");
+        }
+    }
+})
+
+router.post("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+})
--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const morgan = require("morgan");
+const db = require("./db");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sessionStore = new SequelizeStore({ db });
+const passport = require("passport");
 const path = require("path");
 const PORT = process.env.PORT || 1008;
 const app = express();
 
 module.exports = app;
+
+// passport registration
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await db.models.user.findByPk(id);
+        done(null, user);
+    } catch (err) {
+        done(err);
+    }
+});
 
 const createApp = () => {
     // logging middleware
@@ -13,6 +29,23 @@ const createApp = () => {
     // body parsing middleware
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
+
+    // sessions middleware
+    app.use(
+        session({
+            secret: process.env.SESSION_STORE || "w6unYWSDgcE4VXubn6ec",
+            store: sessionStore,
+            resave: false,
+            saveUninitialized: false
+        })
+    )
+
+    // authentication middleware
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    // routes
+    app.use("/auth", require("./auth"));
 
     // static file-serving middleware
     app.use(express.static(path.join(__dirname, "..", "public")));
@@ -47,7 +80,10 @@ const startListening = () => {
     );
 }
 
+const syncDb = () => db.sync();
+
 async function startApp() {
+    syncDb();
     createApp();
     startListening();
 }
