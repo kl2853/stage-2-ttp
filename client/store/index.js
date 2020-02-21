a6b089d60e820a34afc7294afc52aa32c6f3ff93
Kat Le <kl2853@barnard.edu>
2020-02-21 12:09:53 -0500

    refactor: rename redux user state for clarity

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,12 @@ import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import user from "./user";
+import iex from "./iex";
 
-const reducer = combineReducers({ user });
+const reducer = combineReducers({ 
+    userState: user,
+    stockState: iex
+ });
 const middleware = composeWithDevTools(
     applyMiddleware(thunkMiddleware, createLogger({ collapsed : true }))
 );
