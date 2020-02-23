825299774725af56705cb689dc6b425baca29b63
Kat Le <kl2853@barnard.edu>
2020-02-22 21:35:08 -0500

    style: rename authform component for consistency

--- client/components/index.js
+++ client/components/index.js
@@ -1,4 +1,4 @@
-export { Login, Signup } from "./auth-form";
+export { Login, Signup } from "./authform";
 export { default as Portfolio } from "./portfolio";
 export { default as NavBar } from "./navbar";
 export { default as SearchBar } from "./searchbar";
